7f72e23d2af9af9bc0f0743b80688936fd077ea9
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-04-10 14:14:18 -0400

    Refactor Desired Chain to Active Chain
    Refactor of the ThirdwebProvider component property from `desiredChainId` to `activeChain`
    
    - Used "mumbai" for consistency across repos

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,12 @@ import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mainnet;
+const activeChain = "mumbai";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThirdwebProvider
-      desiredChainId={activeChainId}
+      activeChain={activeChain}
       authConfig={{
         domain: "example.org",
         authUrl: "/api/auth",
