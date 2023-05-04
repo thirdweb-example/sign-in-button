3d1cc6664b227718ab058067ca7bc8de879efb25
Bankk <106103625+BankkRoll@users.noreply.github.com>
2023-05-04 19:04:05 -0400

    Cleanup: Remove unused import

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 
 // This is the chainId your dApp will work on.
